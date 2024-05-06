4e76c9353214cd493cbc93df3eea91197025ecfe
Enion Ismaili <120396545+enionismaili@users.noreply.github.com>
2024-05-06 20:11:43 +0000

    app improved

---
+++ script.js
@@ -0,0 +1,26 @@
+const inputBox = document.getElementById("input");
+const listContainer = document.getElementById("list");
+
+function addTask() {
+    if(inputBox.value === '') {
+        alert("No task entered!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tag.Name === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
